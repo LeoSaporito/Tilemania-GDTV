46cb09f439968b30ba5606d12935b928a646946d
Leo21897041 <leosaporito16@gmail.com>
2026-05-29 12:30:11 -0400

    Player can move left to right

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,30 +4,30 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 directionalInput;
-    //Rigidbody2D myRigidbody2D;
+    Rigidbody2D myRigidbody2D;
 
-    //[SerializeField] float moveSpeed;
+    [SerializeField] float moveSpeed;
 
     void Start()
     {
-        //myRigidbody2D = GetComponent<Rigidbody2D>();
+        myRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        //Run();
+        Run();
     }
 
-    void OnMove(InputValue value)
+    public void OnMove(InputValue value)
     {
         directionalInput = value.Get<Vector2>();
 
         Debug.Log(directionalInput);
     }
 
-    //void Run()
-    //{
-    //    Vector2 playerVelocity = new Vector2(directionalInput.x, 0);
-    //    myRigidbody2D.linearVelocity = playerVelocity * moveSpeed;
-    //}
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(directionalInput.x, 0);
+        myRigidbody2D.linearVelocity = playerVelocity * moveSpeed;
+    }
 }
